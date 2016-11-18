b1a3a929ac59b03e85faa67e33fa59e96a7bc9b2
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2016-11-18 15:38:12 +0100

    loading JSON file with translations

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ secret view webapp main file
 """
 
 ##libraries
+import json
 import bottle
 from bottle import run
 from beaker.middleware import SessionMiddleware
@@ -12,6 +13,20 @@ import config
 import app_utils
 import routes
 
+#language file loading
+def load_language(ll):
+    lang_locale = 'en' if (ll == '') else ll
+    lang_file_path = "./i18n/" + lang_locale + ".json"
+    f = open(lang_file_path, "rb")
+    data = f.read()
+    f.close()
+    decoded_data = data.decode('utf-8')
+    json_obj = json.loads(str(decoded_data))
+    #print(json_obj)
+    return json_obj
+
+load_language('fr')
+
 #session configuration
 session_opts = {
     'session.type': 'file',
@@ -19,7 +34,6 @@ session_opts = {
     'session.data_dir': './data',
     'session.auto': True
 }
-app = SessionMiddleware(bottle.app(), session_opts)
-bottle.run(app=app)
 
-run(host='localhost', port=config.general['port'], debug=config.general['reloader'], reloader=config.general['reloader'])
+app = SessionMiddleware(bottle.app(), session_opts)
+run(app=app, host='localhost', port=config.general['port'], debug=config.general['reloader'], reloader=config.general['reloader'])
