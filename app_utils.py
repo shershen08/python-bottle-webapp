38d59f1e011dcd338aa68c97648a634b64d6405d
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2016-11-20 20:03:13 +0100

    language loading moved to utils

--- app_utils.py
+++ app_utils.py
@@ -3,6 +3,7 @@ app-wide utils
 """
 import bcrypt
 import api_key
+import json
 
 password = api_key.encryption_salt
 
@@ -16,3 +17,16 @@ def crypt_string(str_text):
     #http://www.mindrot.org/projects/py-bcrypt/
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
     print(hashed)
+
+
+#language file loading
+def load_language(ll):
+    lang_locale = 'en' if (ll == '') else ll
+    lang_file_path = "./i18n/" + lang_locale + ".json"
+    f = open(lang_file_path, "rb")
+    data = f.read()
+    f.close()
+    decoded_data = data.decode('utf-8')
+    json_obj = json.loads(str(decoded_data))
+    #print(json_obj)
+    return json_obj
--- index.py
+++ index.py
@@ -3,7 +3,6 @@ secret view webapp main file
 """
 
 ##libraries
-import json
 import bottle
 from bottle import run
 from beaker.middleware import SessionMiddleware
@@ -13,19 +12,7 @@ import config
 import app_utils
 import routes
 
-#language file loading
-def load_language(ll):
-    lang_locale = 'en' if (ll == '') else ll
-    lang_file_path = "./i18n/" + lang_locale + ".json"
-    f = open(lang_file_path, "rb")
-    data = f.read()
-    f.close()
-    decoded_data = data.decode('utf-8')
-    json_obj = json.loads(str(decoded_data))
-    #print(json_obj)
-    return json_obj
-
-load_language('fr')
+app_utils.load_language('fr')
 
 #session configuration
 session_opts = {
