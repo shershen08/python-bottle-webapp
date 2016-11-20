bd07085485976fb9f2880fd90e07a568c919fab5
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2016-11-20 20:11:09 +0100

    added /api/ states and restructured

--- routes.py
+++ routes.py
@@ -7,7 +7,11 @@ import config
 import app_utils
 
 import bottle
-from bottle import request, route, template, static_file, get
+from bottle import request, route, template, static_file, get, post
+
+########################
+#       main app pages
+########################
 
 @get('/')
 def home():
@@ -23,38 +27,20 @@ def home():
     
     return template('home_page')
 
-@get('/admin')
-def admin(name='admin'):
-    '''
-    admin UI
-    '''
-    #working with session
-    s = bottle.request.environ.get('beaker.session')
-    s['test'] = 'admin visited'
-    s.save()
-
-    print(s)
-
-    name = 'tdsdsest'
-    return template('admin_template', name=name)
-
-
 @get('/create')
 def create():
     '''
-    adding new secret
+    display page to create
     '''
-    plugins.shorten_url.get_short_link(config.demo_link)
-    created_data = controllers.create.process_creation(config.demo_link)
-    user_readable_id = app_utils.encrypt_id(created_data['db_id'])
+
     #return "Added new: " + user_readable_id
-    return template('create_template', qrbase64=created_data['qrbase64'], link=user_readable_id)
+    #return template('create_template', qrbase64=created_data['qrbase64'], link=user_readable_id)
 
 
 @get('/secret/<name:re:[a-z0-9]+>')
 def secret(name):
     '''
-    try to open secret
+    display page to open secret
     '''
     system_readable_id = app_utils.decrypt_id(name)
     if(system_readable_id != None):
@@ -79,8 +65,58 @@ def static_page(page):
     text = 'tdsdsest tdsdsest tdsdsesttdsdsest tdsdsest tdsdsest'
     return template('static_page', page_title=title, page_text=text)
 
-# handling static files
+
+
+########################
+#   internal api
+########################
+
+@post('/api/create')
+def api_create():
+    '''
+    adding new secret request
+    '''
+    
+    # return {
+    #     JSON object
+    # }
+
+@post('/api/read')
+def api_read():
+    '''
+    read secret request
+    '''
+    plugins.shorten_url.get_short_link(config.demo_link)
+    created_data = controllers.create.process_creation(config.demo_link)
+    user_readable_id = app_utils.encrypt_id(created_data['db_id'])
+    # return {
+    #     JSON object
+    # }
+
+
+########################
+#   handling static files
+########################
+
 @get('/assets/<filepath:path>')
 def static(filepath):
     print(filepath)
     return static_file(filepath, root='assets')
+
+
+## todo, maybe for later
+
+# @get('/admin')
+# def admin(name='admin'):
+#     '''
+#     admin UI
+#     '''
+#     #working with session
+#     s = bottle.request.environ.get('beaker.session')
+#     s['test'] = 'admin visited'
+#     s.save()
+
+#     print(s)
+
+#     name = 'tdsdsest'
+#     return template('admin_template', name=name)
